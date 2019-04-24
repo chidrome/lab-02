9d4cfbab0e96a6245d9ce423364fc1b623e6c399
Matt Burger <matt.burger1@outlook.com>
2019-04-24 09:49:57 -0700

    Feature 2: created filterPictures() that returns an array of elements of the user selected keyword

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 'use strict'
 
+var filterPictures = (arr) => {
+  let selectValue = $('.class').val();
+  let usrSelect = [];
+  arr.forEach(element => {
+    if(element.keyword === selectValue){
+      usrSelect.push(element);
+    }
+  });
+  return usrSelect;
+}
