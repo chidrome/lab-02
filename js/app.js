827ca665e88ef2a84a8bc200f2fafcf1fdfff3bf
chidrome <chidrome@gmail.com>
2019-04-24 09:54:34 -0700

    added constructor function

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
 'use strict'
+//global variables
+var imageObjects = [];
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+  $.get('../data/page-1.json', (data, status) => {
+    data.forEach((imgObj) => {
+      imageObjects.push(imgObj)
+    })
+  })
+  console.log(imageObjects);
+
+})
+
+function ImaageObject(description, horns, imgUrl, keyword, title){
+  this.description = description,
+  this.horns = horns, 
+  this.imgUrl = imgUrl,
+  this.keyword = keyword,
+  this.title = title
+};
 
