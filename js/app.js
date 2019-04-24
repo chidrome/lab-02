6f45a182a51ef485dab44e9b2fc8307793df3696
Matt Burger <matt.burger1@outlook.com>
2019-04-24 10:08:26 -0700

    matt and paulo merge result

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
   $.get('../data/page-1.json', (data, status) => {
     data.forEach((imgObj) => {
-      imageObjects.push(new ImageObject(imgObj.description, imgObj.horns, imgObj.image_url, imgObj.keyword, imgObj.title))
+      imageObjects.push(imgObj)
     })
   })
   console.log(imageObjects);
@@ -16,22 +16,18 @@ document.addEventListener('DOMContentLoaded', ()=> {
 function ImageObject(description, horns, imgUrl, keyword, title){
   this.description = description,
   this.horns = horns, 
-  this.image_url = imgUrl,
+  this.imgUrl = imgUrl,
   this.keyword = keyword,
   this.title = title
 };
 
-<<<<<<< HEAD
 var filterPictures = (arr) => {
   let selectValue = $('.class').val();
   let usrSelect = [];
-  arr.forEach(element => {
-    if(element.keyword === selectValue){
+  $.each(arr, function(key, value){
+    if(key.keyword === selectValue){
       usrSelect.push(element);
     }
   });
   return usrSelect;
 }
-=======
-
->>>>>>> ee3f4476be04f84e2bf1d78a68b0360c6e39aab8
